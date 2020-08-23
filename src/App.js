04a8e0d58d6aa3fbcae0d47b32c9b3e902055177
code <mmahad280@gmail.com>
2020-08-23 13:39:47 -0400

    added input and button to msn app and checked the logic for input fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+	//! USESTATE
+
+	const [input, setInput] = useState("");
+	const [messages, setMessages] = useState([]);
+
+	console.log(input);
+	// this will show that the input is being recieved in the console
+
+	console.log(messages);
+	// this will show that
+	// [a] all inputs are being stored in the 'messages' array
+	// [b] new inputs are submitted, they are added to the end of inputs, which are already in the 'messages' array
+	// [c] when the send message button is pressed, the input field is cleared and made blank
+
+	const sendMessage = (event) => {
+		// all the logic to send messages
+		setMessages([...messages, input]);
+		// store all the previous inputs in the messages array
+		// add new inputs to the end of the messages array
+		setInput("");
+		// after send message is clicked,
+		// clear the messages from the input,
+		// and set the input field to be blank
+	};
+
+	//! USEFFECT
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and bounce to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<h1> Hello world </h1>
+
+			{/* input field */}
+			<input value={input} onChange={(event) => setInput(event.target.value)} />
+
+			{/* button */}
+			<button onClick={sendMessage}>send messages</button>
+
+			{/* messages themselves */}
 		</div>
 	);
 }
