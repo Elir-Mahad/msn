b361838c643cf4c16af1661b5ab780bb4ca6ad7b
code <mmahad280@gmail.com>
2020-08-23 14:09:18 -0400

    wrapped the input and button in the form tags. So now a message can be submitted when the user presses enter after typing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
 	//! USESTATE
+	// state allows you to make a change on the page without refreshing the whole page
+	// via the react dom
 
 	const [input, setInput] = useState("");
 	const [messages, setMessages] = useState([]);
@@ -17,6 +19,11 @@ function App() {
 	// [c] when the send message button is pressed, the input field is cleared and made blank
 
 	const sendMessage = (event) => {
+		event.preventDefault();
+		// By default, the input field gets refreshed every time something is submitted
+		// This line of code stops the input field from refereshing.
+		// As a result, the previous messages that were inputed, will not be erased.
+
 		// all the logic to send messages
 		setMessages([...messages, input]);
 		// store all the previous inputs in the messages array
@@ -34,12 +41,26 @@ function App() {
 			<h1> Hello world </h1>
 
 			{/* input field */}
-			<input value={input} onChange={(event) => setInput(event.target.value)} />
-
-			{/* button */}
-			<button onClick={sendMessage}>send messages</button>
-
+			<form>
+				<input
+					value={input}
+					onChange={(event) => setInput(event.target.value)}
+				/>
+				{/* button */}
+				<button type="submit" onClick={sendMessage}>
+					send messages
+				</button>
+			</form>
 			{/* messages themselves */}
+			{/* adding submit="type" and wrapping the input and button in a form tag, 
+      allows us to send a message, by pressing the enter after we finish typing
+      in the input field. */}
+
+			{messages.map((message) => (
+				// loop through the messages array and
+				<p>{message}</p>
+				// display each message in the messages array
+			))}
 		</div>
 	);
 }
